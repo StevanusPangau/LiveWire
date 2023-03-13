e3dc051f6cdd093ccacb1a81c08ba2977312089d
Stevanus Pangau <stevanuspangau50@gmail.com>
2023-03-13 14:55:14 +0700

    data binding

--- app/Http/Livewire/HelloWorld.php
+++ app/Http/Livewire/HelloWorld.php
@@ -6,10 +6,14 @@ use Livewire\Component;
 
 class HelloWorld extends Component
 {
+    public $name = "Evan";
+
+    public $loud = false;
+
+    public $greeting = ['Hello'];
+
     public function render()
     {
-        return view('livewire.hello-world', [
-            'name' => 'Evan',
-        ]);
+        return view('livewire.hello-world');
     }
 }
--- resources/views/livewire/hello-world.blade.php
+++ resources/views/livewire/hello-world.blade.php
@@ -1,3 +1,26 @@
 <div>
-    Hello World {{ $name }}
+    {{-- tambahkan .debounce untuk mengatur datanya negeload berapa lama --}}
+    {{-- <input wire:model.debounce.1000ms="name" type="text"> --}}
+
+    {{-- tambahkan .lazy untuk ngerender data pada saat user klik diluar input --}}
+    {{-- <input wire:model.lazy="name" type="text"> --}}
+
+    <input wire:model="name" type="text">
+
+    <input wire:model="loud" type="checkbox">
+
+    <select wire:model="greeting" multiple>
+        <option>Hello</option>
+        <option>Goodbye</option>
+        <option>Adios</option>
+    </select>
+
+
+    {{-- Hello {{ strtoupper($name) }} --}}
+    {{-- Hello {{ $name }} --}}
+    {{ implode(', ', $greeting) }} {{ $name }}
+
+    @if ($loud)
+        !
+    @endif
 </div>
